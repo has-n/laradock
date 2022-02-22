3cf73679daf62db9c8b0a30bb6d30b6366f4bbe6
Hasan Khan <hasan@akkompany.com>
2022-02-21 22:09:54 -0800

    Changes

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-letsencrypt certonly --webroot -w /var/www -d "$CN" --agree-tos --email "$EMAIL" --non-interactive --text --dry-run
+letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$CN" --agree-tos --email "$EMAIL" --non-interactive --text --dry-run
 
 cp /etc/letsencrypt/archive/"$CN"/cert1.pem /var/certs/cert1.pem
 cp /etc/letsencrypt/archive/"$CN"/chain1.pem /var/certs/chain1.pem
